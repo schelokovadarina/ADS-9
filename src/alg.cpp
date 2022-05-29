c0349deea13e51ac6c0affd0225b6cdd6831a2f3
schelokovadarina <99324863+schelokovadarina@users.noreply.github.com>
2022-05-29 22:25:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -13,7 +13,7 @@ if (!file) {
 std::cout << "Error opening for reading" << std::endl;
 return Tree;
 }
-const char word = "";
+const std::string word = "";
 while (!file.eof()) {
 char character = file.get();
 if ('A' <= character && character <= 'Z') {
